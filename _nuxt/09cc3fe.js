9b6598612c221220eeab3eb955909559f3e4b401
diana-kot <57452102+diana-kot@users.noreply.github.com>
2023-12-03 22:39:25 +0400

    Updates

---
+++ _nuxt/09cc3fe.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{411:function(n,t,e){"use strict";e.r(t);var l={name:"NuxtTutorial"},o=e(26),component=Object(o.a)(l,(function(){return(0,this._self._c)("div",[this._v("Начальная страница")])}),[],!1,null,null,null);t.default=component.exports}}]);
